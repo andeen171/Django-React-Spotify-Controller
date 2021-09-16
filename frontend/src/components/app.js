c98e9d442b75221e1522e217f6b3d0324f759759
andeen171 <73368853+andeen171@users.noreply.github.com>
2021-09-15 21:19:07 -0300

    Updated React and added post api endpoints

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,12 +1,18 @@
 import React, {Component} from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return (<h1>Reações</h1>);
+        return (
+            <div>
+                <HomePage/>
+            </div>
+        );
     }
 }
 
